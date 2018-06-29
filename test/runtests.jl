a6555c62d139869b6f5e1cedb8f30906d3e20d88
SimonDanisch <sdanisch@gmail.com>
2018-06-29 19:30:03 +0200

    don't run any tests on user PC

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
 using WinRPM
 using Test
-WinRPM.install("gcc", yes = true)
-gcc = joinpath(WinRPM.installdir, "usr", string(Sys.ARCH, "-w64-mingw32"), "sys-root", "mingw", "bin", "gcc.exe")
 
-@test success(`$gcc --version`)
+if haskey(ENV, "CI") && haskey(ENV, "WINRPM_DO_THE_TEST") && ENV["WINRPM_DO_THE_TEST"] == "true"
+    WinRPM.install("gcc", yes = true)
+    gcc = joinpath(WinRPM.installdir, "usr", string(Sys.ARCH, "-w64-mingw32"), "sys-root", "mingw", "bin", "gcc.exe")
+
+    @test success(`$gcc --version`)
+end
