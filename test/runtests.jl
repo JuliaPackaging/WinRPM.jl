96deef8f0bbb2bf1b496895713363b6765e70519
jcheyns <cheyns.jan@gmail.com>
2018-10-17 16:15:10 -0500

    elementary test on escaped names

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,15 @@
-#using Test
+using Test
 using WinRPM
+using   Libz, LibExpat#\, URIParser
 
-#@testset "WinRPM" begin
+@testset "WinRPM" begin
 
-#    @testset "escaping" begin
-    #libclp = library_dependency("libclp",aliases=["libClp-1"])
-        WinRPM.install("Cbc")
-#        @test true
-#    end;
+#check if a file with a special char is downloaded correctly
+    @testset "escaping" begin
+        todo = WinRPM.Package[]
+        push!(todo, WinRPM.select(WinRPM.lookup("libstdc++6", WinRPM.OS_ARCH),"libstdc++6"))    #push!(todo, select(lookup(pkg, arch), pkg))
+        WinRPM.do_install(WinRPM.Packages(todo))
+        @test true
+    end;
 
-
-
-#end;
+end;
