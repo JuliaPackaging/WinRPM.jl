e3cbe796761631ec68ae0cfef5ff741fcd325b44
Milan Bouchet-Valat <nalimilan@club.fr>
2018-10-18 11:20:03 +0200

    Cleanup

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,11 @@
 using Test
 using WinRPM
-using   Libz, LibExpat#\, URIParser
+using Libz, LibExpat
 
-@testset "WinRPM" begin
-
-#check if a file with a special char is downloaded correctly
-    @testset "escaping" begin
-        todo = WinRPM.Package[]
-        push!(todo, WinRPM.select(WinRPM.lookup("libstdc++6", WinRPM.OS_ARCH),"libstdc++6"))    #push!(todo, select(lookup(pkg, arch), pkg))
-        WinRPM.do_install(WinRPM.Packages(todo))
-        @test true
-    end;
-
-end;
+#check that a file with a special char is downloaded correctly
+@testset "escaping" begin
+    todo = WinRPM.Package[]
+    push!(todo, WinRPM.select(WinRPM.lookup("libstdc++6", WinRPM.OS_ARCH), "libstdc++6"))
+    WinRPM.do_install(WinRPM.Packages(todo))
+    @test true
+end
