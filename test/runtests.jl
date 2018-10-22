b6934864ac52af44f2a0ca29eba277e95a1deee9
jcheyns <cheyns.jan@gmail.com>
2018-10-22 12:28:07 -0500

    test if installed

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,8 +4,10 @@ using Libz, LibExpat
 
 #check that a file with a special char is downloaded correctly
 @testset "escaping" begin
-    todo = WinRPM.Package[]
-    push!(todo, WinRPM.select(WinRPM.lookup("libstdc++6", WinRPM.OS_ARCH), "libstdc++6"))
-    WinRPM.do_install(WinRPM.Packages(todo))
+    testPkgs = WinRPM.Package[]
+    push!(testPkgs, WinRPM.select(WinRPM.lookup("libstdc++6", WinRPM.OS_ARCH), "libstdc++6"))
+    WinRPM.do_install(WinRPM.Packages(testPkgs))
+    todo, toup = WinRPM.prepare_install(testPkgs[1])
+    @test( isempty(todo) && isempty(toup))
     @test true
 end
